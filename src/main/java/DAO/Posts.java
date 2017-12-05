8c0be28a34cafa47270dd6143ce5b3da65857c97
Xinchen <sinkozsinkoz@gmail.com>
2017-12-05 14:14:16 -0800

    added models

--- src/main/java/DAO/Posts.java
+++ src/main/java/DAO/Posts.java
@@ -3,5 +3,21 @@ package DAO;
 /**
  * Created by Xinchen on 5/12/17.
  */
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Posts {
+
 }
